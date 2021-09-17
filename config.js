55958bb87978566b2772c41b8741e8f5ed4f2551
pduyennn <89238911+pduyennn@users.noreply.github.com>
2021-09-17 20:57:30 +0700

    pd.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Chào Sơn xoăn iu dấu!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnYes: 'Vẫn cứ là iu em <33',
+    btnNo: 'Không, tình iu à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại  yêu em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'zì em ngoan hiền, em quá nà tuỵt zời',
+    mess: 'Em biết mà. Yêu anh nhìu nhìu 😘😘',
+    messDesc: 'Khi nào rảnh ùi thì nhớ kiu em ùi minh đi ăng sinh nhật nhe.',
+    btnAccept: 'Okiiiii lun  <3',
+    messLink: 'https://www.facebook.com/messages/t/100009076513841/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
